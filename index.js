1213918917737c25410aae3df70b60f9c1d1ed60
Daniel Sousa <daniel.lgdn@gmail.com>
2024-09-23 22:18:11 -0300

    chore: add pg and setup

---
+++ index.js
@@ -0,0 +1,12 @@
+import 'dotenv/config.js'
+import express from 'express'
+import { postgresHelper } from './src/db/postgres/helper.js'
+
+const app = express()
+
+app.get('/', async (req, res) => {
+    const results = await postgresHelper.query('Select * from users;')
+    res.send(JSON.stringify(results))
+})
+
+app.listen(3000, () => console.log('Listening on port 3000'))
