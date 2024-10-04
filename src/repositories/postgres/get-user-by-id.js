39e761ac86588c6fef755ebdd491b37cc3b4e14d
Daniel Sousa <daniel.lgdn@gmail.com>
2024-10-04 00:03:20 -0300

    fix: fix in src/repositories/postgres/get-user-by-id.js PostgresHelper

--- src/repositories/postgres/get-user-by-id.js
+++ src/repositories/postgres/get-user-by-id.js
@@ -1,8 +1,8 @@
-import { PostgresHelper } from '../../db/postgres/helper.js'
+import { postgresHelper } from '../../db/postgres/helper.js'
 
 export class PosgresGetUserByIdRepository {
     async execute(userId) {
-        const user = await PostgresHelper.query(
+        const user = await postgresHelper.query(
             'SELECT * FROM users WHERE id = $1',
             [userId],
         )
