7ba84798e8f2dd120199e2af8edb4e1a689f3b2c
Daniel Sousa <daniel.lgdn@gmail.com>
2024-10-22 22:29:12 -0300

    feat: add  UpdateUSeCase

---
+++ src/use-cases/update-user.js
@@ -0,0 +1,40 @@
+import bcrypt from 'bcrypt'
+import { EmailAlreadyInUseError } from '../erros/users.js'
+import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js'
+import { PostgresUpdateUserRepository } from '../repositories/postgres/update-user'
+
+export class UpdteUserUsecase {
+    async execute(userId, updateUserParams) {
+        if (updateUserParams.email) {
+            const postgresGetUserByEmailRepository =
+                new PostgresGetUserByEmailRepository()
+
+            const userAlredExists =
+                await postgresGetUserByEmailRepository.execute(
+                    updateUserParams.email,
+                )
+
+            if (userAlredExists) {
+                throw new EmailAlreadyInUseError(updateUserParams.email)
+            }
+        }
+
+        const user = { ...updateUserParams }
+
+        if (updateUserParams.password) {
+            const hashedPassword = await bcrypt.hash(
+                updateUserParams.password,
+                10,
+            )
+
+            user.password = hashedPassword
+        }
+        const postgresUpdateUserRepository = new PostgresUpdateUserRepository()
+
+        const updateUser = await postgresUpdateUserRepository.execute(
+            userId,
+            updateUserParams,
+        )
+        return updateUser
+    }
+}
